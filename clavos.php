7e2cc3803ec35e20401982ce0814b46ee85f0857
Treighton <treighton@pagedesigngroup.com>
2019-11-22 11:42:48 -0800

    initial

---
+++ clavos.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name:     PDG - HBH Farm Search
+ * Description:     Custom plugin for Farmers Veteran Coalition to add a HBH farm search tool
+ * Author:          Treighton @ Page Design Group
+ * Text Domain:     PDG_hbh
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         PDG_hbh
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die();
+}
+
+include __DIR__ . '/includes/cpts.php';
+
+/**
+ * This is an example of how to create a post type using the CPT factory
+ */
+
+$news = new Custom_Post_Type( 'News' );
+// $news->add_taxonomy('News Type');
+// $meetings = new Custom_Post_Type('Meetings');
